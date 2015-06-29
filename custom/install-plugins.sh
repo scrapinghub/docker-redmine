27afb4194a29f87ef3fa86b32fb64e0df351413f
Luar Roji <luar@scrapinghub.com>
2015-06-29 21:05:38 +0200

    Added Scrapinghub own theme

--- custom/install-plugins.sh
+++ custom/install-plugins.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-set -e
+#!/bin/bash -e
 
 git_clone() {
     REPO="$1"
@@ -8,7 +6,7 @@ git_clone() {
     DST_DIR="$3"
 
     if [ -z $DST_DIR ]; then
-        DST_DIR="`echo $REPO | sed 's/.*\/\(.*\)$/\\1/'`"
+        DST_DIR="`echo $REPO | sed 's/.*\/\(.*\)$/\\1/' | tr '-' '_'`"
     fi
 
     if [ -d $DST_DIR ]; then
@@ -19,6 +17,16 @@ git_clone() {
     echo "Cloning repo $REPO on directory: $DST_DIR"
 
     bash -c "git clone $REPO $DST_DIR && cd $DST_DIR && git checkout -qf $VERSION_HASH"
+}
+
+download_or_clone() {
+    if [[ "$1" =~ ".zip" ]]; then
+        echo "Installing $d ..."
+        TMPFILE=`mktemp`
+        curl -s $d > $TMPFILE && unzip -n $TMPFILE && rm $TMPFILE
+    else
+        git_clone $@
+    fi
 }
 
 cd assets/plugins
@@ -32,15 +40,24 @@ git_clone https://github.com/redcloak/redmine_s3 a3c98ae3373f5e139b92ffebed08263
 git_clone https://github.com/a-ono/redmine_per_project_formatting ced76f76ee59b17e951c35616d96e84552fd8028
 git_clone https://github.com/koppen/redmine_github_hook a0f2d924dac4c0d68b68eda774f732c45133a646
 
+
 if [ -n "$*" ]; then
     for d in $*; do
-        echo "Installing $d ..."
-        TMPFILE=`mktemp`
-        curl -s $d > $TMPFILE && unzip -n $TMPFILE && rm $TMPFILE
+        if [[ ! "$d" =~ "theme" ]]; then
+            download_or_clone `echo $d | tr '=' ' '`
+        fi
     done
 fi
 
 cd ../../assets/themes
+
+if [ -n "$*" ]; then
+    for d in $*; do
+        if [[ "$d" =~ "theme" ]]; then
+            download_or_clone `echo $d | tr '=' ' '`
+        fi
+    done
+fi
 
 for i in \
     https://github.com/lqez/redmine-theme-basecamp-with-icon     \

33969640df94081ef3e1c95297feefc586c0675f
Luar Roji <luar@scrapinghub.com>
2015-08-04 20:07:09 +0100

    Use our forked version of redmine_mentions

--- custom/install-plugins.sh
+++ custom/install-plugins.sh
@@ -33,7 +33,7 @@ cd assets/plugins
 
 git_clone https://github.com/scrapinghub/redmine_didyoumean c264073809d1a74f3efe0ff6fd80114f413dc822
 git_clone https://github.com/credativUK/redmine_image_clipboard_paste 1bb13068433706b603a7a7302c0182f78c3d34c3
-git_clone https://github.com/arkhitech/redmine_mentions 2f3122a9c81a24f4a47e34b72625055e28aed903
+git_clone https://github.com/scrapinghub/redmine_mentions 19abe29da4a3c0adee29721035880796c3346d88
 git_clone https://github.com/sciyoshi/redmine-slack 6129dd0004f38e3263b15431e5b6c2786aff876f redmine_slack
 git_clone https://github.com/redcloak/redmine_s3 a3c98ae3373f5e139b92ffebed082634cc9bfe82
 
